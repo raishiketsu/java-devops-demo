ee3f70b77b1985716c3c821c28d2de17a773a178
shiketsu.rai <shiketsu.rai@gmail.com>
2022-11-05 00:15:23 +0900

    commit 1

---
+++ src/test/java/com/raii/devops/JavaDevopsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raii.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDevopsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
