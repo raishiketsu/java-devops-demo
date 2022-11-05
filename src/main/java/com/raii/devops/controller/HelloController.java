d75a1ab63b0f1f0a487217f475dbd8cf6ebbe980
raishiketsu <37066555+raishiketsu@users.noreply.github.com>
2022-11-05 20:11:58 +0900

    Update HelloController.java

--- src/main/java/com/raii/devops/controller/HelloController.java
+++ src/main/java/com/raii/devops/controller/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello(){
-        return "Hello Java DevOps!!!  66666";
+        return "Hello Java DevOps!!!  888";
     }
 
 
