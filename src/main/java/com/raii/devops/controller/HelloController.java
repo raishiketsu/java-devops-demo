697ec4abc7a26c3314baf326c1e123ef7c1b5a4a
shiketsu.rai <shiketsu.rai@gmail.com>
2022-11-05 11:07:37 +0900

    update java

--- src/main/java/com/raii/devops/controller/HelloController.java
+++ src/main/java/com/raii/devops/controller/HelloController.java
@@ -1,13 +1,15 @@
-package com.raii.devops.controller;
+package com.itdachang.devops.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-	public String hello() {
-		return "Hello Java DevOps!";
-	}
+    public String hello(){
+        return "Hello Java DevOps!!!  66666";
+    }
+
 
 }
