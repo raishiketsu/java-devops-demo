f6f3fd73d9e21f47841fd42675833006709e625c
shiketsu.rai <shiketsu.rai@gmail.com>
2022-11-05 20:10:35 +0900

    update controller

--- src/main/java/com/raii/devops/controller/HelloController.java
+++ src/main/java/com/raii/devops/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.itdachang.devops.controller;
+package com.raii.devops.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
